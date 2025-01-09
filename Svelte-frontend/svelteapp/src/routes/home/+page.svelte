c69784473872d969c18e2023f6a764c6ad75ac59
victor matrat <vivicoubar@gmail.com>
2025-01-09 16:00:16 +0100

    Git init

---
+++ Svelte-frontend/svelteapp/src/routes/home/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import type { PageData } from './$types';
+
+    let { data }: { data: PageData } = $props();
+    export const csr = false;
+    // Make server side fetch to 192.168.0.4/5000
+    fetch('http://192.168.0.4:5000')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+
+</script>
+
+<main>
+    <h1>Home</h1>
+    <p>{data}</p>
+</main>
+
+
