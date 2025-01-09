267de0326f8a0085072375db3856ea452cabcc54
victor matrat <vivicoubar@gmail.com>
2025-01-09 16:52:56 +0100

    Test

--- Svelte-frontend/svelteapp/src/routes/+page.server.js
+++ Svelte-frontend/svelteapp/src/routes/+page.server.js
@@ -1,21 +1,22 @@
 /** @type {import('./$types').PageServerLoad} */
+
 export async function load() {
 	try {
-		const response = await fetch('http://192.168.0.4:5000/hour');
+		const response = await fetch('http://127.0.0.1:5000/api/hour');
 
 		if (!response.ok) {
 			throw new Error('Failed to fetch time');
 		}
 
-		const data = await response.json(); // Supposons que l'API retourne un objet { time: "HH:MM:SS" }
-
+		const data = await response.json();
+		console.log(data);
 		return {
-			time: data.time
+			time: await data.hour
 		};
 	} catch (error) {
 		console.error('Error fetching time:', error);
 		return {
-			time: 'Erreur lors de la récupération de l’heure'
+			time: -1
 		};
 	}
 }
--- Svelte-frontend/svelteapp/src/routes/+page.svelte
+++ Svelte-frontend/svelteapp/src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<!-- <h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p> -->
+
+<script>
+	import { page } from "$app/state";
+    let time = page.data.time;
+</script>
+
+<h1>Heure actuelle</h1>
+<p>L’heure est : {time}</p>
