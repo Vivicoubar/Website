a8c012de3987a9bf3b357f3fbb54fcb1c063f581
victor matrat <vivicoubar@gmail.com>
2025-01-16 16:18:23 +0100

    Some work

--- Svelte-frontend/svelteapp/src/routes/login/+page.svelte
+++ Svelte-frontend/svelteapp/src/routes/login/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import { page } from '$app/state';
+	import { backendIp } from '../../store';
 
 	// Variable to store user input values
 	let username: string = '';
@@ -11,7 +11,7 @@
 		event.preventDefault(); // Prevent default form submission
 
 		try {
-			const response = await fetch('http://127.0.0.1:5000/api/login', {
+			const response = await fetch(backendIp + '/api/login', {
 				method: 'POST',
 				headers: {
 					'Content-Type': 'application/json'
